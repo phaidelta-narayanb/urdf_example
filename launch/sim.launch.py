b5ef7b55009becb7588dad9fb752a4ba7f152cb6
Narayan Bandodker <narayan.b@phaidelta.com>
2023-11-29 14:31:07 +0530

    re-added rsp to sim launch

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -11,6 +11,24 @@ from launch_ros.actions import Node
 def generate_launch_description():
     # Configure the node
 
+    # Specify the name of the package and path to xacro file within the package
+    pkg_name = "urdf_example"
+    file_subpath = "description/example_robot.urdf.xacro"
+
+    # Use xacro to process the file
+    xacro_file = os.path.join(get_package_share_directory(pkg_name), file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+    # Configure the node
+    node_robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {"robot_description": robot_description_raw}
+        ],  # add other parameters here if required
+    )
+
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
@@ -28,4 +46,4 @@ def generate_launch_description():
     )
 
     # Run the node
-    return LaunchDescription([gazebo, spawn_entity])
+    return LaunchDescription([gazebo, node_robot_state_publisher, spawn_entity])
