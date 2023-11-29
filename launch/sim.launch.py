6a21f09aa7a767a0276aa0510b70443a60ff62ce
Narayan Bandodker <narayan.b@phaidelta.com>
2023-11-29 14:34:11 +0530

    use sim time flag readded

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -12,22 +12,24 @@ def generate_launch_description():
     # Configure the node
 
     # Specify the name of the package and path to xacro file within the package
-    pkg_name = "urdf_example"
-    file_subpath = "description/example_robot.urdf.xacro"
+    pkg_name = 'urdf_example'
+    file_subpath = 'description/example_robot.urdf.xacro'
+
 
     # Use xacro to process the file
-    xacro_file = os.path.join(get_package_share_directory(pkg_name), file_subpath)
+    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
     robot_description_raw = xacro.process_file(xacro_file).toxml()
+
 
     # Configure the node
     node_robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="screen",
-        parameters=[
-            {"robot_description": robot_description_raw}
-        ],  # add other parameters here if required
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw,
+        'use_sim_time': True}] # add other parameters here if required
     )
+
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
